3ddedb85e9cdd141ebbe110570cacdf5f126d2bc
Štefan Jambrich <stefan.jambrich@student.spsmb.cz>
2021-09-15 15:09:09 +0200

    Money transferer working

--- src/cz/spsmb/a3/Application.java
+++ src/cz/spsmb/a3/Application.java
@@ -1,8 +1,21 @@
 package cz.spsmb.a3;
 
+import java.util.Scanner;
+
 public class Application {
+    public static final int[] PAPER_MONEY = {5000, 1000, 500, 200, 100, 50, 20, 10, 5, 2};
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int moneyIn = sc.nextInt();
+        while (moneyIn != 0){
+            for (int j : PAPER_MONEY) {
+                if (moneyIn - j >= 0) {
+                    moneyIn -= j;
+                    System.out.println(j);
+                }
+            }
+        }
     }
 }
